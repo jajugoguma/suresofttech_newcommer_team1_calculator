78e6028998bfef7781b6f7f282d3372ccccd6242
rinechran <rinechran@outlook.jp>
2020-12-21 14:02:10 +0900

    feat :tcp connect

--- Calculator/ViewModels/MainWindowViewModel.cs
+++ Calculator/ViewModels/MainWindowViewModel.cs
@@ -7,6 +7,7 @@ using System.Windows.Data;
 using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using CalendarNetworkClient;
 
 namespace Calculator.ViewModels
 {
@@ -17,10 +18,14 @@ namespace Calculator.ViewModels
         {
             EqualsEnable = false;
             _logs = new ObservableCollection<Log>();
+            _client = new Client();
         }
 
 
         #region Network
+
+        public Client _client;
+
         //네트워크 변수
         private string _ip;
         public string IP { get { return _ip; } set { SetProperty(ref _ip, value); } }
@@ -40,9 +45,19 @@ namespace Calculator.ViewModels
         private void ExecuteAccess()
         {
             //주어진 ip, port로 서버 엑세스
+            EndPoint endPoint = new EndPoint(IP, Port);
+            _client.SetEndPoint(endPoint);
+            
+            if(_client.Connect() == true)
+            {
+                NetworkState = "입력됨.";
+                EqualsEnable = true;
+            }
+            else
+            {
+                EqualsEnable = false;
+            }
 
-            NetworkState = "입력됨.";
-            EqualsEnable = true;
         }
 
         //사용 중 연결끊길경우 호출
---
+++ calendarNetwork/Client.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Sockets;
+
+namespace CalendarNetworkClient
+{
+
+    public class EndPoint
+    {
+        public string Ip;
+        public int Port;
+        public EndPoint(string ip ,string stringPort)
+        {
+            int port = Convert.ToInt32(stringPort);
+            SetUp(ip, port);
+        }
+        void SetUp(string ip , int port)
+        {
+            Ip = ip;
+            Port = port;
+        }
+    }
+    public class Client
+    {
+        TcpClient _Tcplient;
+        EndPoint _Endpoint;
+        bool _IsConnnect;
+        public Client()
+        {
+            _IsConnnect = false;
+            _Endpoint = null;
+        }
+        public Client(EndPoint endPoint) : this()
+        {
+            SetEndPoint(endPoint);
+        }
+        public void SetEndPoint(EndPoint endPoint)
+        {
+            _Endpoint = endPoint;
+        }
+        public bool Connect()
+        {
+            
+            _Tcplient = new TcpClient(_Endpoint.Ip,_Endpoint.Port);
+            if (_Endpoint == null)
+                return false;
+
+
+            _IsConnnect = true;
+            return true;
+        }
+
+
+
+
+
+    }
+}
