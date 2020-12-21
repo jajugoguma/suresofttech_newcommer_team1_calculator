5cbf63aaa888abaf3bf4605bc6ce2d6a90890887
rary <fnhecl@gmail.com>
2020-12-21 17:01:24 +0900

    checker() 추가

--- Parser/Parser.cpp
+++ Parser/Parser.cpp
@@ -1,11 +1,27 @@
 #include "Node.h"
 #include "ParserTree.h"
 
+bool checker(std::string s) {
+	for (int i = 0; i < s.length(); i++) {
+		if (s[i] >= '(' && s[i] <= '9') {
+			if (s[i] == ',' || s[i] == '.') {
+				return false;
+			}
+			else return true;
+		}
+		else return false;
+	}
+}
 
 int main()
 {
 	std::string str = "";
 	std::cin >> str;
+
+	if (!checker(str)) {
+		std::cout << "잘못된 수식입니다." << std::endl;
+		return 0;
+	}
 
 	mapInit();
 
