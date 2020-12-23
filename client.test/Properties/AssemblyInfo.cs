776ba6d3d2b5fbd4b0941d46bcfc8197d3f8b8ae
rinechran <rinechran@outlook.jp>
2020-12-23 11:57:43 +0900

    test : 테스트 솔루션 제작

---
+++ client.test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("client.test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("client.test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("27936106-4a45-4ba6-9879-81fc02fdbd05")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ client.test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace client.test
+{
+    [TestClass]
+    public class CalendarNetworkClient
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
