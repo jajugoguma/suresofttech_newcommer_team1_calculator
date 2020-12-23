8464bc24506bc73110933764a32213622c4aca7a
정민지 <mintti97@gmail.com>
2020-12-23 10:27:17 +0900

    SendTreeViewerEvnet 연결중

--- Calculator/ViewModels/TreeViewerViewModel.cs
+++ Calculator/ViewModels/TreeViewerViewModel.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿using Prism.Mvvm;
+using Prism.Commands;
+using Prism.Events;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Calculator.Infra.Event;
 namespace Calculator.ViewModels
 {
-    class TreeViewerViewModel
+    class TreeViewerViewModel : BindableBase
     {
+        private IEventAggregator _ea;
+
+        private string _value;
+        public string Value
+        {
+            get { return _value; }
+            set { SetProperty(ref _value, value); }
+        }
+
+        private void SetTreeViewerValue(string value)
+        {
+            _value = value;
+        }
+
+        public TreeViewerViewModel(IEventAggregator ea)
+        {
+            _ea = ea;
+
+            _ea.GetEvent<SendTreeViewerDataEvent>().Subscribe(SetTreeViewerValue);
+        }
+
     }
 }
